d967e38e22d4ff1d99d9a74694dea83e7a77dbc6
dim42 <dimt777@gmail.com>
2016-12-22 00:35:50 +0300

    DemonstrateNoDeadlockTest

--- src/test/java/test/transfer/model/DemonstrateNoDeadlockTest.java
+++ src/test/java/test/transfer/model/DemonstrateNoDeadlockTest.java
@@ -11,15 +11,17 @@ import java.util.concurrent.CountDownLatch;
 
 public class DemonstrateNoDeadlockTest {
     private static final Logger log = LogManager.getLogger();
+
     private static final int NUM_THREADS = 20;
     private static final int NUM_ACCOUNTS = 5;
-    private static final int NUM_ITERATIONS = 1000000;
-    //    private static final int NUM_ITERATIONS = 1;
+    private static final int NUM_ITERATIONS = 1000_000;
+
     private static final Random rnd = new Random();
-    private static AccountDao accountDao = new AccountDao() {
+
+    private static final AccountDao accountDaoStub = new AccountDao() {
         @Override
         public Account findAccount(String accNum) {
-            return new Account(accNum, String.valueOf(rnd.nextInt(1000) + 200000000), null, null, true, String.valueOf(rnd.nextInt(1000) + 300));
+            return new Account(accNum, String.valueOf(rnd.nextInt(1000) + 200_000_000), null, null, true, String.valueOf(rnd.nextInt(1000) + 300));
         }
 
         @Override
@@ -30,13 +32,10 @@ public class DemonstrateNoDeadlockTest {
     public static void main(String[] args) {
         final Account[] accounts = new Account[NUM_ACCOUNTS];
         for (int i = 0; i < accounts.length; i++) {
-            accounts[i] = new Account("num" + i, String.valueOf(rnd.nextInt(1000) + 200000000), null, null, true, String.valueOf(rnd.nextInt
+            accounts[i] = new Account("num" + i, String.valueOf(rnd.nextInt(1000) + 200_000_000), null, null, true, String.valueOf(rnd.nextInt
                     (1000) + 300));
-//            accounts[i].balance = new BigDecimal(rnd.nextInt(1000) + 200);
-//            accounts[i].balance(new BigDecimal(rnd.nextInt(1000) + 200));
         }
         final CountDownLatch latch = new CountDownLatch(NUM_THREADS * NUM_ITERATIONS);
-//        Transfer transfer = new Transfer(null, null, null);
         class TransferThread extends Thread {
             public void run() {
                 for (int i = 0; i < NUM_ITERATIONS; i++) {
@@ -48,13 +47,8 @@ public class DemonstrateNoDeadlockTest {
                         toInd = rnd.nextInt(NUM_ACCOUNTS);
                         toAcct = accounts[toInd];
                     }
-                    BigDecimal amount = new BigDecimal(rnd.nextInt(100));
                     try {
-//                        fromAcct.transferMoney(toAcct, amount);
-//                        fromAcct.transferMoney1(toAcct, amount);
-//                        fromAcct.transferMoney2(toAcct, amount);
-                        new Transfer(accountDao, fromAcct, toAcct, amount).run();
-//                        transfer.run(fromAcct, toAcct, amount);
+                        new Transfer(accountDaoStub, fromAcct, toAcct, new BigDecimal(rnd.nextInt(100))).run();
                     } finally {
                         latch.countDown();
                     }
@@ -74,14 +68,5 @@ public class DemonstrateNoDeadlockTest {
         }
         long duration = System.nanoTime() - start;
         log.info("duration:" + (((float) duration) / 1000_000));
-//        try {
-//            TimeUnit.SECONDS.sleep(5);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println(in1);
-//        System.out.println(in2);
-//        System.out.println(Transfer.in1);
-//        System.out.println(Transfer.in2);
     }
 }
