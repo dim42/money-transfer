b6361eeb8f00a9cb5626abd159c97d4b8266cd13
dim42 <dimt777@gmail.com>
2016-12-22 05:01:07 +0300

    -unused code (methods)

--- src/main/java/test/transfer/model/Account.java
+++ src/main/java/test/transfer/model/Account.java
@@ -24,64 +24,6 @@ public class Account {
         this.limit = new BigDecimal(limit).setScale(2, DOWN);
     }
 
-    // deadlock
-    public void transferMoney1(Account to, BigDecimal transferAmount) {
-        synchronized (this) {
-            synchronized (to) {
-                if (balance != null && balance.compareTo(transferAmount) > 0) {
-                    balance = balance.subtract(transferAmount);
-                    to.balance = to.balance == null ? transferAmount : to.balance.add(transferAmount);
-                }
-            }
-        }
-    }
-
-    public void transferMoney2(Account to, BigDecimal transferAmount) {
-//        System.out.println(this.balance + " " + to.balance + " " + transferAmount);
-        if (balance != null && balance.compareTo(transferAmount) > 0) {
-            balance = balance.subtract(transferAmount);
-            to.balance = to.balance == null ? transferAmount : to.balance.add(transferAmount);
-        }
-//        System.out.println(this.balance + " " + to.balance + "\n");
-    }
-
-//    static AtomicInteger in1 = new AtomicInteger(0);
-//    static AtomicInteger in2 = new AtomicInteger(0);
-
-    public void transferMoney(final Account to, BigDecimal transferAmount) {
-//        in1.incrementAndGet();
-        if (number.equals(to.number)) {
-//            in2.incrementAndGet();
-//            System.out.println("equals");
-            throw new RuntimeException("The same account number.");
-        }
-        if (number.compareTo(to.number) > 0) {
-            synchronized (this) {
-                synchronized (to) {
-                    doTransfer(to, transferAmount);
-                }
-            }
-        } else {
-            synchronized (to) {
-                synchronized (this) {
-                    doTransfer(to, transferAmount);
-                }
-            }
-        }
-    }
-
-    private void doTransfer(Account to, BigDecimal transferAmount) {
-        if (balance != null && balance.compareTo(transferAmount) >= 0) {
-            balance = balance.subtract(transferAmount);
-            to.balance = to.balance == null ? transferAmount : to.balance.add(transferAmount);
-        }
-    }
-//    private void doTransfer(Account toAcct, BigDecimal amount) {
-//        checkInsufficientBalance(amount);
-//        debit(amount);
-//        toAcct.credit(amount);
-//    }
-
     public BigDecimal getBalance() {
         return balance != null ? balance : ZERO;
     }
--- src/main/java/test/transfer/service/Transfer.java
+++ src/main/java/test/transfer/service/Transfer.java
@@ -7,14 +7,10 @@ import java.math.BigDecimal;
 
 public class Transfer {
 
-    private static final Object tieLock = new Object();
     private final AccountDao accountDao;
     private final Account fromAcct;
     private final Account toAcct;
     private final BigDecimal amount;
-
-//    static AtomicInteger in1 = new AtomicInteger(0);
-//    static AtomicInteger in2 = new AtomicInteger(0);
 
     public Transfer(AccountDao accountDao, Account fromAcct, Account toAcct, BigDecimal amount) {
         this.accountDao = accountDao;
@@ -23,123 +19,26 @@ public class Transfer {
         this.amount = amount;
     }
 
-//    public void run(BigDecimal balance) {
-//        BigDecimal fromAmount = fromAcct.getAmount();
-//        if (fromAmount != null && fromAmount.compareTo(balance) > 0) {
-//            fromAcct.setAmount(fromAmount.subtract(balance));
-//            BigDecimal toAmount = toAcct.getAmount();
-//            toAcct.setAmount(toAmount == null ? balance : toAmount.add(balance));
-//        }
-//    }
-
     public void run() {
-//        if (fromAcct.getNumber().equals(toAcct.getNumber())) {
-//            throw new RuntimeException("The same account number.");
-////            return;
-//        }
         if (fromAcct.greater(toAcct)) {
             synchronized (fromAcct) {
                 synchronized (toAcct) {
-//                    in1.incrementAndGet();
                     doTransfer();
                 }
             }
         } else {
             synchronized (toAcct) {
                 synchronized (fromAcct) {
-//                    in2.incrementAndGet();
-//                    doTransfer(fromAcct, toAcct, amount);
                     doTransfer();
                 }
             }
         }
     }
 
-    //    private void doTransfer(Account fromAcct, Account toAcct, BigDecimal amount) {
     private void doTransfer() {
         fromAcct.checkInsufficientBalance(amount);
         fromAcct.debit(amount);
         toAcct.credit(amount);
         accountDao.updateAccountsBalance(fromAcct.getNumber(), fromAcct.getBalance().toString(), toAcct.getNumber(), toAcct.getBalance().toString());
-    }
-
-    public void run(final Account fromAcct, final Account toAcct, final BigDecimal amount) {
-        if (fromAcct.getNumber().equals(toAcct.getNumber())) {
-            throw new RuntimeException("The same account number.");
-//            return;
-        }
-
-        class Helper {
-            private void doTransfer() {
-//                if (fromAcct.balance != null && fromAcct.balance.compareTo(amount) > 0) {
-//                    fromAcct.balance = fromAcct.balance.subtract(amount);
-//                    toAcct.balance = toAcct.balance == null ? amount : toAcct.balance.add(amount);
-//                }
-//                if (fromAcct.balance() != null && fromAcct.balance().compareTo(amount) > 0) {
-//                    fromAcct.balance(fromAcct.balance().subtract(amount));
-//                    toAcct.balance(toAcct.balance() == null ? amount : toAcct.balance().add(amount));
-//                }
-                fromAcct.checkInsufficientBalance(amount);
-                fromAcct.debit(amount);
-                toAcct.credit(amount);
-            }
-        }
-
-//        if (fromAcct.getNumber().compareTo(toAcct.getNumber()) > 0) {
-        if (fromAcct.greater(toAcct)) {
-            synchronized (fromAcct) {
-                synchronized (toAcct) {
-//                    doTransfer(fromAcct, toAcct, amount);
-                    new Helper().doTransfer();
-                }
-            }
-        } else {
-            synchronized (toAcct) {
-                synchronized (fromAcct) {
-                    new Helper().doTransfer();
-                }
-            }
-        }
-//        in1.incrementAndGet();
-    }
-
-
-    public void transferMoney2(final Account fromAcct,
-                               final Account toAcct,
-                               final BigDecimal amount) {
-        class Helper {
-            public void transfer() {
-                if (fromAcct.getBalance().compareTo(amount) < 0) {
-//                    throw new InsufficientFundsException();
-                    throw new RuntimeException("InsufficientFundsException");
-                } else {
-                    fromAcct.debit(amount);
-                    toAcct.credit(amount);
-                }
-            }
-        }
-        int fromHash = System.identityHashCode(fromAcct);
-        int toHash = System.identityHashCode(toAcct);
-        if (fromHash < toHash) {
-            synchronized (fromAcct) {
-                synchronized (toAcct) {
-                    new Helper().transfer();
-                }
-            }
-        } else if (fromHash > toHash) {
-            synchronized (toAcct) {
-                synchronized (fromAcct) {
-                    new Helper().transfer();
-                }
-            }
-        } else {
-            synchronized (tieLock) {
-                synchronized (fromAcct) {
-                    synchronized (toAcct) {
-                        new Helper().transfer();
-                    }
-                }
-            }
-        }
     }
 }
